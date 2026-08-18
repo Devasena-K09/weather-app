e8bf33090237be58e99cde14e259663c182f7e33
Devasena K <devasenak9203@gmail.com>
2026-08-18 13:36:13 +0530

    Weather App v1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +18,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  String city = "Bangalore";
+  String temperature = "--";
+  String condition = "--";
+  IconData weatherIcon = Icons.wb_sunny;
+  bool isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    getWeather("Bengaluru");
+  }
+  final TextEditingController cityController =
+  TextEditingController();
+  Future<void> getWeather(String cityName) async {
+    setState(() {
+      isLoading = true;
+    });
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    const apiKey = "6f8fd7c31016ebdbb2e0b551daf0cf2d";
+    final url = Uri.parse(
+      "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric",
+    );
+
+    final response = await http.get(url);
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      setState(() {
+        city = data["name"];
+        temperature = "${data["main"]["temp"]}°C";
+        condition = data["weather"][0]["main"];
+        isLoading = false;
+
+        if (condition == "Clouds") {
+          weatherIcon = Icons.cloud;
+        } else if (condition == "Rain") {
+          weatherIcon = Icons.umbrella;
+        } else if (condition == "Clear") {
+          weatherIcon = Icons.wb_sunny;
+        } else if (condition == "Thunderstorm") {
+          weatherIcon = Icons.flash_on;
+        } else {
+          weatherIcon = Icons.wb_cloudy;
+        }
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("City not found"),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,33 +95,58 @@ class WeatherScreen extends StatelessWidget {
         title: const Text("Weather App"),
         centerTitle: true,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            if (isLoading)
+              const CircularProgressIndicator(),
+            if (isLoading)
+              const SizedBox(height: 20),
+        Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: cityController,
+          decoration: const InputDecoration(
+            hintText: "Enter City",
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ),
+        ElevatedButton(
+        onPressed: () {
+      getWeather(cityController.text);
+    },
+    child: const Text("Search"),
+    ),
             Icon(
-              Icons.wb_sunny,
+              weatherIcon,
               size: 100,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
+
             Text(
-              "Bangalore",
-              style: TextStyle(
+              city,
+              style: const TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
+
+            const SizedBox(height: 10),
+
             Text(
-              "28°C",
-              style: TextStyle(
+              temperature,
+              style: const TextStyle(
                 fontSize: 50,
               ),
             ),
-            SizedBox(height: 10),
+
+            const SizedBox(height: 10),
+
             Text(
-              "Sunny",
-              style: TextStyle(
+              condition,
+              style: const TextStyle(
                 fontSize: 24,
               ),
             ),
